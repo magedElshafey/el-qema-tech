503c8e80c54284dbab171ba228ef8c60eed09190
megz <magedelshafey98@gmail.com>
2024-09-10 02:01:29 +0300

    removed unused code and assets

--- src/data/data.js
+++ src/data/data.js
@@ -1,44 +1,13 @@
 // contact icons
 import { MdEmail, MdPrivacyTip } from "react-icons/md";
-import {
-  FaMobileAlt,
-  FaFacebook,
-  FaWhatsapp,
-  FaInstagram,
-  FaLocationArrow,
-  FaRocket,
-  FaMedal,
-  FaPencilRuler,
-  FaRegCheckCircle,
-} from "react-icons/fa";
+import { FaMobileAlt, FaLocationArrow } from "react-icons/fa";
 // clients assets
-import client1 from "../assets/part-01.png";
-import client2 from "../assets/part-02.png";
-import client3 from "../assets/part-03.png";
-import client4 from "../assets/part-04.png";
-import client5 from "../assets/part-05.png";
-import client6 from "../assets/part-06.png";
+
 // why us
 import { PiLampPendantBold } from "react-icons/pi";
 import { IoSpeedometerSharp } from "react-icons/io5";
 import { AiFillLike } from "react-icons/ai";
-// orgnizations
-import government from "../assets/government-building.png";
-import health from "../assets/healthcare.png";
-import marketting from "../assets/digital-campaign.png";
-import onlineShop from "../assets/online-shop.png";
-import education from "../assets/classroom.png";
-import software from "../assets/update.png";
-import realstte from "../assets/realstate.png";
-import investor from "../assets/real-estate-market.png";
 
-// services
-import mobile from "../assets/mobile-app.png";
-import server from "../assets/server.png";
-import ui from "../assets/ui.png";
-import program from "../assets/coding.png";
-import seo from "../assets/search-engine-optimization.png";
-import video from "../assets/multimedia.png";
 // portfolio
 import port1 from "../assets/port-01.webp";
 import port2 from "../assets/port-02.webp";
@@ -73,39 +42,7 @@ import port30 from "../assets/port-30.webp";
 import port31 from "../assets/port-31.webp";
 import port32 from "../assets/port-32.webp";
 import port33 from "../assets/port-33.webp";
-// team
-import team1 from "../assets/team-01.png";
-import team2 from "../assets/team-02.png";
-import team3 from "../assets/team-03.png";
-import team4 from "../assets/team-04.png";
-import team5 from "../assets/team-05.png";
-import team6 from "../assets/team-06.png";
-import team7 from "../assets/team-07.png";
-import team8 from "../assets/team-08.png";
-import team9 from "../assets/team-09.png";
-import team10 from "../assets/team-10.png";
-import team11 from "../assets/team-11.png";
-import team12 from "../assets/team-12.png";
-import team13 from "../assets/team-13.png";
-import team14 from "../assets/team-14.png";
-import team15 from "../assets/team-15.png";
-import team16 from "../assets/team-16.png";
-// blogs
-import blog1 from "../assets/blog-01.webp";
-import blog2 from "../assets/blog-02.webp";
-import blog3 from "../assets/blog-03.webp";
-import blog4 from "../assets/blog-04.webp";
-import blog5 from "../assets/blog-05.webp";
-import blog6 from "../assets/blog-06.webp";
-import blog7 from "../assets/blog-07.webp";
-import blog8 from "../assets/blog-08.webp";
-import blog9 from "../assets/blog-09.webp";
-import blog10 from "../assets/blog-10.webp";
-import blog11 from "../assets/blog-11.webp";
-import blog12 from "../assets/blog-12.webp";
-import blog13 from "../assets/blog-13.webp";
-import blog14 from "../assets/blog-11.webp";
-import blog15 from "../assets/blog-15.webp";
+
 // steps
 
 export const contactHeader = {
@@ -122,20 +59,20 @@ export const contactHeader = {
     value: "المنصورة , حي الجامعة",
   },
 };
-export const socialMedia = [
-  {
-    icon: <FaFacebook size={20} />,
-    path: "https://www.facebook.com",
-  },
-  {
-    icon: <FaWhatsapp size={20} />,
-    path: "https://wa.me/+201022153359",
-  },
-  {
-    icon: <FaInstagram size={20} />,
-    path: "https://www.instagram.com",
-  },
-];
+// export const socialMedia = [
+//   {
+//     icon: <FaFacebook size={20} />,
+//     path: "https://www.facebook.com",
+//   },
+//   {
+//     icon: <FaWhatsapp size={20} />,
+//     path: "https://wa.me/+201022153359",
+//   },
+//   {
+//     icon: <FaInstagram size={20} />,
+//     path: "https://www.instagram.com",
+//   },
+// ];
 export const navLinks = [
   {
     title: "home",
@@ -150,7 +87,7 @@ export const navLinks = [
     path: "/works",
   },
 ];
-export const clients = [client1, client2, client3, client4, client5, client6];
+// export const clients = [client1, client2, client3, client4, client5, client6];
 export const whyUs = [
   {
     icon: <PiLampPendantBold size={40} />,
@@ -173,93 +110,93 @@ export const whyUs = [
     desc: "support desc",
   },
 ];
-export const orgnizations = [
-  {
-    img: government,
-    title: "government",
-  },
-  {
-    img: marketting,
-    title: "marketting",
-  },
-  {
-    img: education,
-    title: "education",
-  },
-  {
-    img: onlineShop,
-    title: "onlineShop",
-  },
-  {
-    img: realstte,
-    title: "realstte",
-  },
-  {
-    img: health,
-    title: "health",
-  },
-  {
-    img: investor,
-    title: "investor",
-  },
-  {
-    img: software,
-    title: "software",
-  },
-];
-export const services = [
-  {
-    icon: software,
-    title: "website",
-    desc: "lorem",
-  },
-  {
-    icon: onlineShop,
-    title: "e-commerceee",
-    desc: "lorem",
-  },
-  {
-    icon: mobile,
-    title: "mobile application",
-    desc: "lorem",
-  },
-  {
-    icon: server,
-    title: "hosting",
-    desc: "lorem",
-  },
-  {
-    icon: ui,
-    title: "ui and analysis",
-    desc: "lorem",
-  },
+// export const orgnizations = [
+//   {
+//     img: government,
+//     title: "government",
+//   },
+//   {
+//     img: marketting,
+//     title: "marketting",
+//   },
+//   {
+//     img: education,
+//     title: "education",
+//   },
+//   {
+//     img: onlineShop,
+//     title: "onlineShop",
+//   },
+//   {
+//     img: realstte,
+//     title: "realstte",
+//   },
+//   {
+//     img: health,
+//     title: "health",
+//   },
+//   {
+//     img: investor,
+//     title: "investor",
+//   },
+//   {
+//     img: software,
+//     title: "software",
+//   },
+// ];
+// export const services = [
+//   {
+//     icon: software,
+//     title: "website",
+//     desc: "lorem",
+//   },
+//   {
+//     icon: onlineShop,
+//     title: "e-commerceee",
+//     desc: "lorem",
+//   },
+//   {
+//     icon: mobile,
+//     title: "mobile application",
+//     desc: "lorem",
+//   },
+//   {
+//     icon: server,
+//     title: "hosting",
+//     desc: "lorem",
+//   },
+//   {
+//     icon: ui,
+//     title: "ui and analysis",
+//     desc: "lorem",
+//   },
 
-  {
-    icon: education,
-    title: "e-learning",
-    desc: "lorem",
-  },
-  {
-    icon: marketting,
-    title: "markettingg",
-    desc: "lorem",
-  },
-  {
-    icon: program,
-    title: "programms",
-    desc: "lorem",
-  },
-  {
-    icon: seo,
-    title: "seo optmization",
-    desc: "lorem",
-  },
-  {
-    icon: video,
-    title: "motion grahpich",
-    desc: "lorem",
-  },
-];
+//   {
+//     icon: education,
+//     title: "e-learning",
+//     desc: "lorem",
+//   },
+//   {
+//     icon: marketting,
+//     title: "markettingg",
+//     desc: "lorem",
+//   },
+//   {
+//     icon: program,
+//     title: "programms",
+//     desc: "lorem",
+//   },
+//   {
+//     icon: seo,
+//     title: "seo optmization",
+//     desc: "lorem",
+//   },
+//   {
+//     icon: video,
+//     title: "motion grahpich",
+//     desc: "lorem",
+//   },
+// ];
 export const portfolios = [
   {
     img: port1,
@@ -460,199 +397,199 @@ export const portfolios = [
     id: 33,
   },
 ];
-export const team = [
-  team1,
-  team2,
-  team3,
-  team4,
-  team5,
-  team6,
-  team7,
-  team8,
-  team9,
-  team10,
-  team11,
-  team12,
-  team13,
-  team14,
-  team15,
-  team16,
-];
-export const blogs = [
-  {
-    id: 1,
-    img: blog1,
-    title: "شركة تصميم موشن جرافيك",
-    desc: "شركة رقي أفضل شركة تصميم موشن جرافيك في الكويت، الآن تقدم كافة خدمات الموشن جرافيك بأفضل تكلفة ممكنة ، تواصل ....",
-    day: 24,
-    month: "فبراير",
-  },
-  {
-    id: 2,
-    img: blog2,
-    title: "تصميم موقع الكتروني لبيع المنتجات",
-    desc: "هل تفكر في تصميم موقع الكتروني لبيع المنتجات ؟ ترغب في أن يكون لك تواجد ونشاط على الإنترنت لتسهل على ....",
-    day: 24,
-    month: "فبراير",
-  },
-  {
-    id: 3,
-    img: blog3,
-    title: "انشاء تطبيق تواصل اجتماعي",
-    desc: "إن انشاء تطبيق تواصل اجتماعي يعد من الأفكار والمشاريع المربحة في عصرنا الحالي والدليل على ذلك هو فقط النظر إلى ....",
-    day: 24,
-    month: "فبراير",
-  },
-  {
-    id: 4,
-    img: blog4,
-    title: "أفضل شركات تصميم المواقع",
-    desc: "إذا كنت تبحث عن أفضل شركات تصميم المواقع في الكويت فـ شركة رقي هي الأفضل بلا منازع ، تحتل شركة ....",
-    day: 24,
-    month: "فبراير",
-  },
-  {
-    id: 5,
-    img: blog5,
-    title: "تصميم واجهة تطبيق",
-    desc: "تصميم واجهة تطبيق معناها بكل بساطة هو أن يتم تصميم الشكل الظاهري للتطبيق من حيث الألوان وشكل الأيقونات وكافة العناصر ....",
-    day: 24,
-    month: "فبراير",
-  },
-  {
-    id: 6,
-    img: blog6,
-    title: "تصميم موقع سياحي",
-    desc: "لا شك أن تصميم موقع سياحي يستطيع من خلاله عملاءك إتمام عمليات الحجز ، والتعرف على أسعار الرحلات ، وجمع ....",
-    day: 24,
-    month: "فبراير",
-  },
-  {
-    id: 7,
-    img: blog7,
-    title: "أسعار تصميم التطبيقات",
-    desc: "نظراً لارتباط الأفراد في السنوات الماضية بهواتفهم الذكية أصبحت فكرة تصميم تطبيق فكرة ملحة على رواد الأعمال الذين يريدون تقديم ....",
-    day: 24,
-    month: "فبراير",
-  },
-  {
-    id: 8,
-    img: blog8,
-    title: "خطوات انشاء متجر الكتروني",
-    desc: "إذا كنت تريد التعرف على خطوات انشاء متجر الكتروني فعليك بقراءة هذا المقال ، فاليوم سوف نقدم لكم أهم المعلومات ....",
-    day: 24,
-    month: "فبراير",
-  },
-  {
-    id: 9,
-    img: blog9,
-    title: "تصميم تطبيق للايفون",
-    desc: "إذا كنت تبحث عن خدمة تصميم تطبيق للايفون ممتازة ومضمونة وبأفضل سعر في الوطن العربي فأنت في المكان المناسب ، ....",
-    day: 24,
-    month: "فبراير",
-  },
-  {
-    id: 10,
-    img: blog10,
-    title: "تصميم تطبيقات الويب",
-    desc: "إن تصميم تطبيقات الويب تعد من أفضل خدماتنا التي نقدمها ، جرب الآن بنفسك الاستعانة بأفضل فريق عمل متخصص في ....",
-    day: 24,
-    month: "فبراير",
-  },
-  {
-    id: 11,
-    img: blog11,
-    title: "تصميم موقع بيع اون لاين",
-    desc: "إذا كنت تحلم بـ تصميم موقع بيع اون لاين (متجر إلكتروني ) بحيث يكون بإمكانك الوصول إلى أكبر عدد ممكن ....",
-    day: 24,
-    month: "فبراير",
-  },
-  {
-    id: 12,
-    img: blog12,
-    title: "تصميم واجهة المستخدم",
-    desc: "تعد خطوة تصميم واجهة المستخدم من الخطوات المهمة عند تصميم موقع إلكتروني أو تصميم تطبيقات الموبايل ، وذلك لأنها الشكل الظاهري ....",
-    day: 24,
-    month: "فبراير",
-  },
-  {
-    id: 13,
-    img: blog13,
-    title: "تصميم تطبيق والربح منه",
-    desc: "في هذا المقال سوف نوضح لكم أفضل طريقة لـ تصميم تطبيق والربح منه ، أصبحت فكرة انشاء تطبيق اندرويد أو ....",
-    day: 24,
-    month: "فبراير",
-  },
-  {
-    id: 14,
-    img: blog14,
-    title: "أسعار الموشن جرافيك",
-    desc: "إن أسعار الموشن جرافيك لدى شركة رقي هي الأفضل بدون شك ، تقدم شركة رقي خدمات الموشن جرافيك بأسعار مناسبة ....",
-    day: 24,
-    month: "فبراير",
-  },
-  {
-    id: 15,
-    img: blog15,
-    title: "تصميم موقع ويب كامل",
-    desc: "هل تحلم بـ تصميم موقع ويب كامل يكون واجهتك الإلكترونية أمام عملائك تستطيع من خلاله أن تصل إلى عدد غير ....",
-    day: 24,
-    month: "فبراير",
-  },
-  {
-    id: 25,
-    img: blog1,
-    title: "شركة تصميم موشن جرافيك",
-    desc: "شركة رقي أفضل شركة تصميم موشن جرافيك في الكويت، الآن تقدم كافة خدمات الموشن جرافيك بأفضل تكلفة ممكنة ، تواصل ....",
-    day: 24,
-    month: "فبراير",
-  },
-];
-export const steps = [
-  {
-    step: 1,
-    title: "إختر الخدمة",
-    desc: "إختر الخدمة المٌناسبة لتحقيق هدفك من بين خدمتنا المُختلفة",
-    icon: <FaRegCheckCircle size={20} />,
-    enTitle: "select the service",
-    enDesc:
-      "Select the most suitable service to meet your needs from our diverse offerings.",
-  },
-  {
-    step: 2,
-    title: "عرض السعر و التعاقد",
-    desc: "فريقنا من الخبراء في مجالاتهم يسعون دائماً في مساعدة عملاءنا على تحقيق أهدافهم.",
-    icon: <FaPencilRuler size={20} />,
-    enDesc:
-      "We offer competitive pricing and flexible contracts. Our team of experts is dedicated to helping our clients achieve their goals.",
-    enTitle: "Price quote and contract",
-  },
-  {
-    step: 3,
-    title: "رحلة التحول الرقمي",
-    desc: "نحرص على تزويدك بالحلول الرقمية المبتكرة، معتمدين على خبراتنا الكبيرة في مجال تكنولوجيا المعلومات",
-    icon: <FaMedal size={20} />,
-    enTitle: "Digital transformation journey",
-    enDesc:
-      "We are keen to provide you with innovative digital solutions, relying on our extensive experience in the field of information technology",
-  },
-  {
-    step: 4,
-    title: "الإنطلاق",
-    desc: "نسعى دائماً لخدمة عملاؤنا على مدار الساعة ، كما نقوم بتدريب فريق العمل لديك",
-    icon: <FaRocket size={20} />,
-    enTitle: "Launch",
-    enDesc:
-      "We always strive to serve our customers around the clock, and we also train your work team",
-  },
-];
-export const statistics = [
-  {
-    number: 50,
-    text: "موظف",
-  },
-  {
-    number: 120,
-    text: "مشروع",
-  },
-];
+// export const team = [
+//   team1,
+//   team2,
+//   team3,
+//   team4,
+//   team5,
+//   team6,
+//   team7,
+//   team8,
+//   team9,
+//   team10,
+//   team11,
+//   team12,
+//   team13,
+//   team14,
+//   team15,
+//   team16,
+// ];
+// export const blogs = [
+//   {
+//     id: 1,
+//     img: blog1,
+//     title: "شركة تصميم موشن جرافيك",
+//     desc: "شركة رقي أفضل شركة تصميم موشن جرافيك في الكويت، الآن تقدم كافة خدمات الموشن جرافيك بأفضل تكلفة ممكنة ، تواصل ....",
+//     day: 24,
+//     month: "فبراير",
+//   },
+//   {
+//     id: 2,
+//     img: blog2,
+//     title: "تصميم موقع الكتروني لبيع المنتجات",
+//     desc: "هل تفكر في تصميم موقع الكتروني لبيع المنتجات ؟ ترغب في أن يكون لك تواجد ونشاط على الإنترنت لتسهل على ....",
+//     day: 24,
+//     month: "فبراير",
+//   },
+//   {
+//     id: 3,
+//     img: blog3,
+//     title: "انشاء تطبيق تواصل اجتماعي",
+//     desc: "إن انشاء تطبيق تواصل اجتماعي يعد من الأفكار والمشاريع المربحة في عصرنا الحالي والدليل على ذلك هو فقط النظر إلى ....",
+//     day: 24,
+//     month: "فبراير",
+//   },
+//   {
+//     id: 4,
+//     img: blog4,
+//     title: "أفضل شركات تصميم المواقع",
+//     desc: "إذا كنت تبحث عن أفضل شركات تصميم المواقع في الكويت فـ شركة رقي هي الأفضل بلا منازع ، تحتل شركة ....",
+//     day: 24,
+//     month: "فبراير",
+//   },
+//   {
+//     id: 5,
+//     img: blog5,
+//     title: "تصميم واجهة تطبيق",
+//     desc: "تصميم واجهة تطبيق معناها بكل بساطة هو أن يتم تصميم الشكل الظاهري للتطبيق من حيث الألوان وشكل الأيقونات وكافة العناصر ....",
+//     day: 24,
+//     month: "فبراير",
+//   },
+//   {
+//     id: 6,
+//     img: blog6,
+//     title: "تصميم موقع سياحي",
+//     desc: "لا شك أن تصميم موقع سياحي يستطيع من خلاله عملاءك إتمام عمليات الحجز ، والتعرف على أسعار الرحلات ، وجمع ....",
+//     day: 24,
+//     month: "فبراير",
+//   },
+//   {
+//     id: 7,
+//     img: blog7,
+//     title: "أسعار تصميم التطبيقات",
+//     desc: "نظراً لارتباط الأفراد في السنوات الماضية بهواتفهم الذكية أصبحت فكرة تصميم تطبيق فكرة ملحة على رواد الأعمال الذين يريدون تقديم ....",
+//     day: 24,
+//     month: "فبراير",
+//   },
+//   {
+//     id: 8,
+//     img: blog8,
+//     title: "خطوات انشاء متجر الكتروني",
+//     desc: "إذا كنت تريد التعرف على خطوات انشاء متجر الكتروني فعليك بقراءة هذا المقال ، فاليوم سوف نقدم لكم أهم المعلومات ....",
+//     day: 24,
+//     month: "فبراير",
+//   },
+//   {
+//     id: 9,
+//     img: blog9,
+//     title: "تصميم تطبيق للايفون",
+//     desc: "إذا كنت تبحث عن خدمة تصميم تطبيق للايفون ممتازة ومضمونة وبأفضل سعر في الوطن العربي فأنت في المكان المناسب ، ....",
+//     day: 24,
+//     month: "فبراير",
+//   },
+//   {
+//     id: 10,
+//     img: blog10,
+//     title: "تصميم تطبيقات الويب",
+//     desc: "إن تصميم تطبيقات الويب تعد من أفضل خدماتنا التي نقدمها ، جرب الآن بنفسك الاستعانة بأفضل فريق عمل متخصص في ....",
+//     day: 24,
+//     month: "فبراير",
+//   },
+//   {
+//     id: 11,
+//     img: blog11,
+//     title: "تصميم موقع بيع اون لاين",
+//     desc: "إذا كنت تحلم بـ تصميم موقع بيع اون لاين (متجر إلكتروني ) بحيث يكون بإمكانك الوصول إلى أكبر عدد ممكن ....",
+//     day: 24,
+//     month: "فبراير",
+//   },
+//   {
+//     id: 12,
+//     img: blog12,
+//     title: "تصميم واجهة المستخدم",
+//     desc: "تعد خطوة تصميم واجهة المستخدم من الخطوات المهمة عند تصميم موقع إلكتروني أو تصميم تطبيقات الموبايل ، وذلك لأنها الشكل الظاهري ....",
+//     day: 24,
+//     month: "فبراير",
+//   },
+//   {
+//     id: 13,
+//     img: blog13,
+//     title: "تصميم تطبيق والربح منه",
+//     desc: "في هذا المقال سوف نوضح لكم أفضل طريقة لـ تصميم تطبيق والربح منه ، أصبحت فكرة انشاء تطبيق اندرويد أو ....",
+//     day: 24,
+//     month: "فبراير",
+//   },
+//   {
+//     id: 14,
+//     img: blog14,
+//     title: "أسعار الموشن جرافيك",
+//     desc: "إن أسعار الموشن جرافيك لدى شركة رقي هي الأفضل بدون شك ، تقدم شركة رقي خدمات الموشن جرافيك بأسعار مناسبة ....",
+//     day: 24,
+//     month: "فبراير",
+//   },
+//   {
+//     id: 15,
+//     img: blog15,
+//     title: "تصميم موقع ويب كامل",
+//     desc: "هل تحلم بـ تصميم موقع ويب كامل يكون واجهتك الإلكترونية أمام عملائك تستطيع من خلاله أن تصل إلى عدد غير ....",
+//     day: 24,
+//     month: "فبراير",
+//   },
+//   {
+//     id: 25,
+//     img: blog1,
+//     title: "شركة تصميم موشن جرافيك",
+//     desc: "شركة رقي أفضل شركة تصميم موشن جرافيك في الكويت، الآن تقدم كافة خدمات الموشن جرافيك بأفضل تكلفة ممكنة ، تواصل ....",
+//     day: 24,
+//     month: "فبراير",
+//   },
+// ];
+// export const steps = [
+//   {
+//     step: 1,
+//     title: "إختر الخدمة",
+//     desc: "إختر الخدمة المٌناسبة لتحقيق هدفك من بين خدمتنا المُختلفة",
+//     icon: <FaRegCheckCircle size={20} />,
+//     enTitle: "select the service",
+//     enDesc:
+//       "Select the most suitable service to meet your needs from our diverse offerings.",
+//   },
+//   {
+//     step: 2,
+//     title: "عرض السعر و التعاقد",
+//     desc: "فريقنا من الخبراء في مجالاتهم يسعون دائماً في مساعدة عملاءنا على تحقيق أهدافهم.",
+//     icon: <FaPencilRuler size={20} />,
+//     enDesc:
+//       "We offer competitive pricing and flexible contracts. Our team of experts is dedicated to helping our clients achieve their goals.",
+//     enTitle: "Price quote and contract",
+//   },
+//   {
+//     step: 3,
+//     title: "رحلة التحول الرقمي",
+//     desc: "نحرص على تزويدك بالحلول الرقمية المبتكرة، معتمدين على خبراتنا الكبيرة في مجال تكنولوجيا المعلومات",
+//     icon: <FaMedal size={20} />,
+//     enTitle: "Digital transformation journey",
+//     enDesc:
+//       "We are keen to provide you with innovative digital solutions, relying on our extensive experience in the field of information technology",
+//   },
+//   {
+//     step: 4,
+//     title: "الإنطلاق",
+//     desc: "نسعى دائماً لخدمة عملاؤنا على مدار الساعة ، كما نقوم بتدريب فريق العمل لديك",
+//     icon: <FaRocket size={20} />,
+//     enTitle: "Launch",
+//     enDesc:
+//       "We always strive to serve our customers around the clock, and we also train your work team",
+//   },
+// ];
+// export const statistics = [
+//   {
+//     number: 50,
+//     text: "موظف",
+//   },
+//   {
+//     number: 120,
+//     text: "مشروع",
+//   },
+// ];
