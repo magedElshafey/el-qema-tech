4a711e3ca2b0c545b5a5ca2dbad11ad1495b3038
magedElshafey <magedelshafey98@gmail.com>
2024-09-11 05:02:52 -0700

    create the work details page

--- src/components/common/loader/Spinner.jsx
+++ src/components/common/loader/Spinner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import style from "./spinner.module.css";
 const Spinner = () => {
   return (
-    <div className="w-screen h-screen fixed z-[1500] flex items-center justify-center">
+    <div className="w-screen h-screen fixed top-0 left-0 bg-white z-[1500] flex items-center justify-center">
       <div className={style.progress}></div>
     </div>
   );
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -5,7 +5,7 @@ import { useQuery } from "react-query";
 import Spinner from "../components/common/loader/Spinner";
 import { useTranslation } from "react-i18next";
 import { FaArrowLeft, FaArrowRightLong } from "react-icons/fa6";
-
+import logo from "../assets/bird_2.jpg";
 const Work = () => {
   const { t, i18n } = useTranslation();
   const params = useParams();
@@ -21,7 +21,7 @@ const Work = () => {
         <Spinner />
       ) : (
         <div>
-          <div className="w-full h-[200px] bg-black text-white flex items-center justify-center text-md md:text-lg lg:text-xl xl:text-2xl font-black mb-8 lg:mb-12">
+          <div className="w-full h-[120px] md:h-[200px] bg-black text-white flex items-center justify-center text-md md:text-lg lg:text-xl xl:text-2xl font-black mb-8 lg:mb-12">
             <div className="flex items-center gap-2">
               <p>{t("home")}</p>
               <p>
@@ -51,6 +51,7 @@ const Work = () => {
                   loading="lazy"
                   className="w-[250px] h-[300px]"
                 />
+
                 {/* to do : slider */}
               </div>
               <div>
@@ -67,6 +68,55 @@ const Work = () => {
                   <p className="text-white p-3 bg-blue flex items-center justify-center rounded-md w-[180px]">
                     {data?.data?.data?.solution}
                   </p>
+                </div>
+                <div className="py-5 border-b border-bslate-600">
+                  <div className="flex items-center gap-1 mb-3">
+                    <p className="font-black">{t("project date")} :</p>
+                    <p className="text-slate-500">25-10-2023</p>
+                  </div>
+                  <div className="flex items-center gap-1 mb-3">
+                    <p className="font-black">{t("visit the website")} :</p>
+                    <a
+                      className="text-slate-500 underline"
+                      href="https://www.google.com"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {t("visit the website")}
+                    </a>
+                  </div>
+                  <div className="flex items-center gap-1 mb-3">
+                    <p className="font-black">{t("type")} :</p>
+                    <p className="text-slate-500">{t("websitee")}</p>
+                  </div>
+                  <div className="flex items-center gap-1 mb-3">
+                    <p className="font-black">{t("status")} :</p>
+                    <p className="text-slate-500">
+                      {t("available on the internet")}
+                    </p>
+                  </div>
+                </div>
+                <div className="mt-5">
+                  <h2 className="font-bold mb-4 text-base md:text-md lg:text-lg ">
+                    {t("client information")}
+                  </h2>
+                  <div className="flex items-center gap-4 lg:gap-8">
+                    <p className="text-base md:text-md xl:text-lg font-black text-blue">
+                      {data?.data?.data?.title}
+                    </p>
+                    <img
+                      loading="lazy"
+                      src={logo}
+                      alt={data?.data?.data?.title}
+                      className="h-[80px] w-[80px]"
+                    />
+                  </div>
+                  <div>
+                    <p className=" font-semibold">{t("about the client")} : </p>
+                    <p className="text-slate-500 w-full md:w-1/2">
+                      {t("lorem").slice(0, 150)}
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
